ed3e7ed35842ffdb9d95885c0b18a79c581ae558
Zoe Blair <zoe.blair@rtraction.com>
2014-03-06 11:08:38 -0500

    Adding GA code to footer

--- wp-content/themes/boilerplate/footer.php
+++ wp-content/themes/boilerplate/footer.php
@@ -60,6 +60,16 @@
         </div>
         <?php endif; ?>
 		   <div class="copyright">Copyright &copy; <?php date('Y'); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> Yoga Wellness. All rights reserved. <a href ="<?php echo get_permalink(2); ?> "></div>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-40649472-1');ga('send','pageview');
+        </script>
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
